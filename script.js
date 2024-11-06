582c253bbcca2feabdb982c6d4d05441b96b2f82
Logan Vaughan <LBVaughan25@gmail.com>
2024-11-06 10:28:48 -0500

    second

---
+++ script.js
@@ -0,0 +1,11 @@
+
+const buttonClick = (event) =>{
+    event.preventDefault();
+    
+    let email = document.getElementById('email');
+    let date = document.getElementById('date');
+    let name = document.getElementById('name');
+    let recipeType = document.getElementById('recipeType');
+
+
+}
